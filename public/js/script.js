69a8e6fd6ff46001a706ae62d0fa8b04e19df1be
Diego <diegocomisso@gmail.com>
2025-12-03 22:23:42 -0300

    nueva navbar

--- public/js/script.js
+++ public/js/script.js
@@ -1,1 +1,13 @@
 console.log("index cargado correctamente");
+
+const nav = document.getElementById("nav")
+const abrir = document.getElementById("abrir")
+const cerrar = document.getElementById("cerrar")
+
+abrir.addEventListener("click", ()=> {
+    nav.classList.add("visible");
+})
+
+cerrar.addEventListener("click", ()=>{
+    nav.classList.remove("visible");
+})
