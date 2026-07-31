cbbfea7aff45aa1dedd2fb84732d5e31c980c335
Diego <diegocomisso@gmail.com>
2026-07-31 00:19:02 -0300

    reemplazado archivo middlewares/archivo.js con la version commonjs

--- src/middlewares/archivos.js
+++ src/middlewares/archivos.js
@@ -1,27 +1,29 @@
 // Procesa los archivos subidos en las peticiones
-import fs from "fs";
-import { join, dirname } from "path";
-import { fileURLToPath } from "url";
-import multer from "multer";
+const fs = require("fs");
+const { join } = require("path");
+const multer = require("multer");
 
 // Configuración de Multer para almacenar los archivos en la carpeta 'uploads'
-const storage = multer.diskStorage({ //guardamos en el alamcenamiento local
+const storage = multer.diskStorage({
+    // guardamos en el almacenamiento local
     destination: function (req, file, cb) {
-        const path = join(
-            dirname(fileURLToPath(import.meta.url)),
-            "../../public/uploads"
-        );
+        const path = join(__dirname, "../../public/uploads");
+
         // Crear la carpeta si no existe
-        //Nos aseguramos que la carpeta va a estar, github borra carpetas vacias y con esto lo solucionamos
+        // Nos aseguramos que la carpeta va a estar, github borra carpetas vacías y con esto lo solucionamos
         fs.mkdirSync(path, { recursive: true });
+
         cb(null, path);
     },
+
     filename: function (req, file, cb) {
-        const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
+        const uniqueSuffix =
+            Date.now() + "-" + Math.round(Math.random() * 1e9);
+
         cb(null, uniqueSuffix + "-" + file.originalname);
     },
 });
 
 const upload = multer({ storage: storage });
 
-export default upload;
+module.exports = upload;
