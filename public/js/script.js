21b4ab532309c2b2feabd40be94e74003a4d11f3
Diego <diegocomisso@gmail.com>
2026-08-01 16:20:27 -0300

    Mejorado accesibilidad en index.html

--- public/js/script.js
+++ public/js/script.js
@@ -1,47 +1,39 @@
 console.log("index cargado correctamente");
 
+const usuario = JSON.parse(localStorage.getItem("usuario"));
+const loginLink = document.getElementById("loginLink");
+const registerLink = document.getElementById("registerLink");
+const logoutLink = document.getElementById("logoutLink");
+
 const nav = document.getElementById("nav")
 const abrir = document.getElementById("abrir")
 const cerrar = document.getElementById("cerrar")
 
+if (usuario) {
+    loginLink.textContent = usuario.nombre;
+    loginLink.href = "#";
+
+    registerLink.hidden = true;
+    logoutLink.hidden = false;
+}
+
+if (usuario && loginLink) {
+    loginLink.textContent = usuario.nombre;
+    loginLink.href = "profile.html";
+}
+
+logoutLink.addEventListener("click", (e) => {
+    e.preventDefault();
+    localStorage.removeItem("usuario");
+    window.location.reload();
+});
+
 abrir.addEventListener("click", () => {
     nav.classList.add("visible");
+    abrir.setAttribute("aria-expanded", "true");
 })
 
 cerrar.addEventListener("click", () => {
     nav.classList.remove("visible");
+    abrir.setAttribute("aria-expanded", "false");
 })
-
-
-const images = [
-    "assets/s01.png",
-    "assets/s02.png",
-    "assets/s03.png",
-    "assets/s04.png",
-    "assets/s05.png",
-    "assets/s06.png",
-    "assets/s07.png",
-    "assets/s10.png",
-    "assets/s11.png",
-    "assets/s12.png",
-    "assets/s13.png",
-    "assets/s14.png",
-    "assets/s29.png",
-    "assets/s27.png",
-];
-
-function changeHeroImage() {
-    const img = document.getElementById("hero-img");
-    const img2 = document.getElementById("hero-img2");
-    const random = Math.floor(Math.random() * images.length);
-    const random2 = random + 1;
-    img.src = images[random];
-    img2.src = images[random2];
-    console.log(random);
-}
-
-// inicial
-changeHeroImage();
-
-// cambia cada 5s
-setInterval(changeHeroImage, 5000);
