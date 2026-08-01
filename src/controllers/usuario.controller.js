57a2f116281a4354708e35c05f2b9eb2c3e9235f
Diego <diegocomisso@gmail.com>
2026-08-01 12:45:16 -0300

    Agregado try y catch en usuario.controller

--- src/controllers/usuario.controller.js
+++ src/controllers/usuario.controller.js
@@ -3,120 +3,160 @@ const prisma = require("../config/prisma");
 
 // ---------------------------------------------------------
 async function access(req, res) {
-    const data = req.body;
+    try {
+        const data = req.body;
 
-    if (!data.email || !data.clave) {
-        return res
-            .status(400)
-            .send({ msg: "Faltan datos obligatorios", error: true });
+        if (!data.email || !data.clave) {
+            return res.status(400).json({
+                error: true,
+                message: "Faltan datos obligatorios",
+            });
+        }
+
+        const user = await prisma.usuarios.findUnique({
+            where: { email: data.email },
+        });
+
+        if (!user) {
+            return res.status(404).json({
+                error: true,
+                message: "Usuario no encontrado",
+            });
+        }
+
+        const isPasswordValid = await bcrypt.compare(data.clave, user.clave);
+
+        if (!isPasswordValid) {
+            return res.status(401).json({
+                error: true,
+                message: "Clave incorrecta",
+            });
+        }
+
+        return res.status(200).send({
+            data: {
+                id: user.id,
+                nombre: user.nombre,
+                email: user.email,
+                es_admin: user.es_admin,
+            },
+        });
+
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({
+            error: true,
+            message: "Error al iniciar sesión",
+        });
     }
-
-    const user = await prisma.usuarios.findUnique({
-        where: { email: data.email },
-    });
-
-    if (!user) {
-        return res
-            .status(404)
-            .send({ msg: "Usuario no encontrado", error: true });
-    }
-
-    const isPasswordValid = await bcrypt.compare(data.clave, user.clave);
-
-    if (!isPasswordValid) {
-        return res
-            .status(401)
-            .send({ msg: "Clave incorrecta", error: true });
-    }
-
-    return res.status(200).send({
-        data: {
-            id: user.id,
-            nombre: user.nombre,
-            email: user.email,
-            es_admin: user.es_admin,
-        },
-    });
 }
 
 // ---------------------------------------------------------
 async function profile(req, res) {
-    const id = req.params.id;
+    try {
+        const id = Number(req.params.id);
 
-    const user = await prisma.usuarios.findUnique({
-        where: { id: Number(id) },
-        include: {
-            ordenes: {
-                include: {
-                    items: true,
+        if (isNaN(id)) {
+            return res.status(400).json({
+                error: true,
+                message: "ID inválido",
+            });
+        }
+
+        const user = await prisma.usuarios.findUnique({
+            where: { id },
+            include: {
+                ordenes: {
+                    include: {
+                        items: true,
+                    },
                 },
             },
-        },
-    });
+        });
 
-    if (!user) {
-        return res
-            .status(404)
-            .send({ msg: "Usuario no encontrado", error: true });
+        if (!user) {
+            return res.status(404).json({
+                error: true,
+                message: "Usuario no encontrado",
+            });
+        }
+        delete user.clave;
+        return res.status(200).json({ data: user });
+
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({
+            error: true,
+            message: "Error al obtener el usuario",
+        });
     }
-
-    return res.status(200).send({ data: user });
 }
 
 // ---------------------------------------------------------
 async function save(req, res) {
-    const data = req.body;
+    try {
+        const data = req.body;
 
-    if (!data.nombre || !data.email || !data.clave) {
-        return res
-            .status(400)
-            .send({ msg: "Faltan datos obligatorios", error: true });
+        if (!data.nombre || !data.email || !data.clave) {
+            return res.status(400).json({
+                error: true,
+                message: "Faltan datos obligatorios",
+            });
+        }
+
+        const regxEmail =
+            /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+
+        if (!regxEmail.test(data.email)) {
+            return res.status(400).json({
+                error: true,
+                message: "Email no válido",
+            });
+        }
+
+        // Validar que la clave tenga al menos una letra mayúscula, una minúscula, un número y un carácter especial
+        const regexClave =
+            /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+
+        if (!regexClave.test(data.clave)) {
+            return res.status(400).json({
+                error: true,
+                message: "La contraseña debe tener al menos 8 caracteres, una mayúscula, una minúscula, un número y un carácter especial.",
+            });
+        }
+
+        // Validar si el email ya existe
+        const existingUser = await prisma.usuarios.findUnique({
+            where: { email: data.email },
+        });
+
+        if (existingUser) {
+            return res.status(400).json({
+                error: true,
+                message: "El email ya está registrado",
+            });
+        }
+
+        const newUser = await prisma.usuarios.create({
+            data: {
+                nombre: data.nombre,
+                email: data.email,
+                clave: await bcrypt.hash(data.clave, 10),
+                es_admin: data.email.endsWith("@admin.com"),
+            },
+        });
+
+        delete newUser.clave;
+
+        return res.status(201).json({ data: newUser });
+
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({
+            error: true,
+            message: "Error al crear el usuario",
+        });
     }
-
-    const regxEmail =
-        /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
-
-    if (!regxEmail.test(data.email)) {
-        return res
-            .status(400)
-            .send({ msg: "Email no válido", error: true });
-    }
-
-    // Validar que la clave tenga al menos una letra mayúscula, una minúscula, un número y un carácter especial
-    const regexClave =
-        /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
-
-    if (!regexClave.test(data.clave)) {
-        return res
-            .status(400)
-            .send({ msg: "Clave no válida", error: true });
-    }
-
-    // Validar si el email ya existe
-    const existingUser = await prisma.usuarios.findUnique({
-        where: { email: data.email },
-    });
-
-    if (existingUser) {
-        return res
-            .status(400)
-            .send({ msg: "El email ya está registrado", error: true });
-    }
-
-    const newUser = await prisma.usuarios.create({
-        data: {
-            nombre: data.nombre,
-            email: data.email,
-            clave: await bcrypt.hash(data.clave, 10),
-            es_admin: data.email.includes("@admin.com") ? true : false,
-        },
-    });
-
-    return res.status(200).send({ data: newUser });
 }
 
-module.exports = {
-    access,
-    profile,
-    save,
-};
+module.exports = { access, profile, save, };
